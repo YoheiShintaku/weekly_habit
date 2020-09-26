5c7ea1640b730ad01e4aa259857dacbc0862938b
yohei <>
2020-09-27 00:33:55 +0900

    bug fix by framelayout

--- app/src/main/java/com/example/weekly_habit/MainActivity.java
+++ app/src/main/java/com/example/weekly_habit/MainActivity.java
@@ -25,11 +25,15 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import java.text.SimpleDateFormat;
 
 /*
  */
 
 public class MainActivity extends AppCompatActivity {
+    private SimpleDatabaseHelper helper = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,40 +44,71 @@ public class MainActivity extends AppCompatActivity {
         //View view = findViewById(R.id.linearLayout);
         //view.setClickable(true);
 
+        // テーブル作成（存在しない場合）
+        helper = new SimpleDatabaseHelper(this);// db接続用
+        createTables();
+
         // コードからFragmentを追加
         addPlanFramgent();
 
     }
+
+    void createTables(){
+        // テーブルがなければ作成
+        String sql;
+
+        // create plan table
+        sql = String.format(
+                "create table if not exists plan ("
+                        + "itemid INTEGER PRIMARY KEY"
+                        + ", isvalid INTEGER"  // ex.: 1  // 有効or無効 編集すると無効化して新しいid発行
+                        + ", name TEXT"  // ex.: "walking"
+                        + ", dow TEXT"  // ex.: "1,3,5"
+                        + ", interval INTEGER"  // ex.: 1  // 週間隔
+                        + ", starttime TEXT"  // ex.: "10:30"
+                        + ", timewidth INTEGER"  // ex.: 15 // minute
+                        + ")"
+        );
+        try(SQLiteDatabase db = helper.getWritableDatabase()) {
+            db.execSQL(sql);
+        }
+
+        // debug: insert test data.
+        // terminalでやる
+        /*
+        int itemid = 0;
+        int isvalid = 1;
+        String name = "test";
+        sql = String.format("insert into plan (itemid, isvalid, name) values ('%d', '%d', %s');", itemid, isvalid, name);
+        try (SQLiteDatabase db = helper.getWritableDatabase()) {
+            db.execSQL(sql);
+        }
+        */
+    }
+
     public void addPlanFramgent(){
         PlanFragment fragment = new PlanFragment();// Fragmentを作成します
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();// Fragmentの追加や削除といった変更を行う際は、Transactionを利用します
         // 新しく追加を行うのでaddを使用します // 他にも、よく使う操作で、replace removeといったメソッドがあります
-        transaction.add(R.id.mainLinearLayout, fragment);// 1つ目の引数は対象のViewGroupのID、2つ目の引数は追加するfragment
+        transaction.add(R.id.topFrameLayout, fragment);// 1つ目の引数は対象のViewGroupのID、2つ目の引数は追加するfragment
         transaction.commit();// 最後にcommitを使用することで変更を反映します
     }
 
-    public void button1_onClick(View view){
-        //Log.d("event","tab1_onClick");
-        Toast.makeText(this, "plan",Toast.LENGTH_SHORT).show();
 
+    public void button1_onClick(View view){
         PlanFragment fragment = new PlanFragment();// Fragmentを作成します
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();// Fragmentの追加や削除といった変更を行う際は、Transactionを利用します
-        transaction.replace(R.id.mainLinearLayout, fragment);// 1つ目の引数は対象のViewGroupのID、2つ目の引数は追加するfragment
+        transaction.replace(R.id.topFrameLayout, fragment);// 1つ目の引数は対象のViewGroupのID、2つ目の引数は追加するfragment
         transaction.commit();// 最後にcommitを使用することで変更を反映します
     }
 
     public void button2_onClick(View view){
-        //Log.d("event","tab1_onClick");
-        Toast.makeText(this, "do",Toast.LENGTH_SHORT).show();
-
         DoFragment fragment = new DoFragment();// Fragmentを作成します
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();// Fragmentの追加や削除といった変更を行う際は、Transactionを利用します
         // 新しく追加を行うのでaddを使用します // 他にも、よく使う操作で、replace removeといったメソッドがあります
-        transaction.replace(R.id.mainLinearLayout, fragment);// 1つ目の引数は対象のViewGroupのID、2つ目の引数は追加するfragment
+        transaction.replace(R.id.topFrameLayout, fragment);// 1つ目の引数は対象のViewGroupのID、2つ目の引数は追加するfragment
         transaction.commit();// 最後にcommitを使用することで変更を反映します
     }
     public void button3_onClick(View view){
-        //Log.d("event","tab1_onClick");
-        Toast.makeText(this, "check",Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/weekly_habit/PlanFragment.java
+++ app/src/main/java/com/example/weekly_habit/PlanFragment.java
@@ -2,12 +2,21 @@ package com.example.weekly_habit;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
 
 // Fragmentクラスを継承
 public class PlanFragment extends Fragment {
+    private SimpleDatabaseHelper helper = null;
+    private LinearLayout planLinearLayout;
 
     // Fragmentで表示するViewを作成するメソッド
     @Override
@@ -15,13 +24,67 @@ public class PlanFragment extends Fragment {
 
         super.onCreateView(inflater, container, savedInstanceState);
 
+        // db接続用
+        helper = new SimpleDatabaseHelper(getContext());
+
+        //ページ番号
+        //int ii = getArguments().getInt("intValue");
+
+        View view = inflater.inflate(R.layout.fragment_plan, null);
+
+        // date
+        setDate(view);  // calendarArray生成
+
+        return view;
         // 先ほどのレイアウトをここでViewとして作成します
-        return inflater.inflate(R.layout.fragment_plan, container, false);
+        //return inflater.inflate(R.layout.fragment_plan, container, false);
     }
 
-    // Viewが生成し終わった時に呼ばれるメソッド
-    @Override
+    void setDate(View view) {
+        TextView textView;
+        int textsize = 30;
+        int gravity = 1;
+        int width = ViewGroup.LayoutParams.MATCH_PARENT;
+        int height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        planLinearLayout = view.findViewById(R.id.planLinearLayout);
+        //Log.d("debug","setDate");
+        for (int i = 0; i < 7; i++) {
+            textView = new TextView(getContext());
+
+            textView.setText(String.valueOf(i));
+            textView.setTextSize(textsize);
+            textView.setGravity(gravity);  // center
+            //textView.setBackground(getResources().getDrawable( R.drawable.view_frame ));
+            textView.setLayoutParams(new ViewGroup.LayoutParams(width, height));
+            planLinearLayout.addView(textView);
+        }
+    }
+
+    /*
+        @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        Log.d("aa","plan 2");
+        int textsize = 20;
+        int wc = ViewGroup.LayoutParams.WRAP_CONTENT;
+        //TextView textViewArray[];
+        //textViewArray = new TextView[7];
+        TextView textView;
+        String str = "aaaaaaaaaa";
+        LinearLayout planLinearLayout = (LinearLayout)view.findViewById(R.id.planLinearLayout);
+        for(int i=0; i<7 ;i++) {
+            textView = new TextView(getContext());
+
+            textView.setText("adfawefawera");
+            textView.setTextSize(textsize);
+            textView.setGravity(1);
+            textView.setBackground(getResources().getDrawable( R.drawable.view_frame));
+            textView.setLayoutParams(new ViewGroup.LayoutParams(wc, wc));
+
+            // LinearLayoutに追加
+            planLinearLayout.addView(textView, new LinearLayout.LayoutParams(0, wc,1));
+            //textViewArray[i] = textView;
+        }
     }
+     */
 }
